4ee18a24e9c6f58179f1adf1f72e76383f12ff70
BVDRCORP <84379805+BVDRCORP@users.noreply.github.com>
2024-09-11 10:08:13 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -40,11 +40,11 @@ def mongraphique():
     return render_template("graphique.html")
 
 
-@app.route('/extract-minutes/<date_string>')
-def extract_minutes(date_string):
-    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-    minutes = date_object.minute
-    return jsonify({'minutes': minutes})
+#@app.route('/extract-minutes/<date_string>')
+#def extract_minutes(date_string):
+#   date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+#    minutes = date_object.minute
+#    return jsonify({'minutes': minutes})
 
 @app.route('/commits/')
 def show_commits():
