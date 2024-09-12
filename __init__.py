653276a8ae9b81d8ecd7ef26ec045b018e53d43e
Yasmina936 <yamghar6@myges.fr>
2024-09-12 15:36:22 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,21 +4,48 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
-app = Flask(__name__)    
+from flask import jsonify
+import json
 
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+                                                                                                                                       
+app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #com 
+    return render_template('hello.html')
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
 
 
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+  
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
+  
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route('/commits/')
+def show_commits():
+    return render_template('commits.html')
+
+@app.route("/commits/")
+def moncommits():
+    return render_template("commits.html")   #com2
   
 if __name__ == "__main__":
   app.run(debug=True)
